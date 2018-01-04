acdda61dafad45f295fadc97f91d30eb88ee1387
paulcosta2001@gmail.com <paulcosta2001@gmail.com>
2018-01-04 08:57:10 -0500

    mainmenu

--- crm.rb
+++ crm.rb
@@ -1,16 +1,29 @@
+require "./contact.rb"
+require "pry"
+
 class CRM
 
   def initialize
-
   end
 
   def main_menu
-
+  while true
+  print_main_menu
+  user_selected = gets.to_i
+  call_option(user_selected)
+  end
   end
 
   def print_main_menu
-
+  puts '[1] Add a new contact'
+  puts '[2] Modify an existing contact'
+  puts '[3] Delete a contact'
+  puts '[4] Display all the contacts'
+  puts '[5] Search by attribute'
+  puts '[6] Exit'
+  puts 'Enter a number: '
   end
+
 
   def call_option
 
@@ -29,11 +42,11 @@ class CRM
   end
 
   def display_all_contacts
-    
+
   end
 
   def search_by_attribute
-    
+
   end
 
 
